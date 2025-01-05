352f89fe5fceebd34be56c20b6d006db1c9d99dd
Bogomil Pankov <87603630+Bogi15@users.noreply.github.com>
2025-01-05 17:34:40 +0200

    Update UserManagement.h
    added new functions and new some essential things

--- UserManagement.h
+++ UserManagement.h
@@ -7,6 +7,23 @@
 
 using namespace std;
 
+struct Meal {
+    string name;
+    int calories;
+};
+
+struct Workout {
+    string name;
+    int calories;
+};
+
+struct DailyLog {
+    string date;
+    vector<Meal> meals;
+    vector<Workout> workouts;
+    int totalCaloriesConsumed = 0;
+    int totalCalorisBurned = 0;
+};
 
 struct User {
     string username;
@@ -15,14 +32,23 @@ struct User {
     string gender;
     double height; // in cm
     double weight; // in kg
+    double BMR;
     string activityLevel;
     string goal;
     string accountType;
+    int calorieDifferenceFromMaintenance = 0;
+    int dailyCalorieGoal;
+    vector<DailyLog> logs;
 };
 
 
 void signUp(vector<User>& users);
 void logIn(const vector<User>& users);
 void closeApp();
+void addMeal(User& user);
+void addWorkout(User& user);
+void viewDailyLog(const User& user);
+void updateUserInfo(User& user);
+int calculateCalorieGoal(const User& user);
 
 #endif 
